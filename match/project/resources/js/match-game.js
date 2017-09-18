5c95140768b6165590d1a8302db962d416d1c39c
Raph <raphg27@hotmail.com>
2017-09-18 17:05:57 +0200

    firstcommit

---
+++ match/project/resources/js/match-game.js
@@ -0,0 +1,35 @@
+var MatchGame = {};
+
+/*
+  Sets up a new game after HTML document has loaded.
+  Renders a 4x4 board of cards.
+*/
+
+/*
+  Generates and returns an array of matching card values.
+ */
+
+MatchGame.generateCardValues = function () {
+  var cards = [[1,1],[2,2],[3,3],[4,4],[5,5],[6,6],[7,7],[8,8]];
+  return cards;
+
+};
+
+/*
+  Converts card values to jQuery card objects and adds them to the supplied game
+  object.
+*/
+
+MatchGame.renderCards = function(cardValues, $game) {
+
+
+};
+
+/*
+  Flips over a given card and checks to see if two cards are flipped over.
+  Updates styles on flipped cards depending whether they are a match or not.
+ */
+
+MatchGame.flipCard = function($card, $game) {
+
+};
